679b6637dc69ec4554069718cf4a42b69183b950
purvjeja <jejapurv@gmail.com>
2024-03-13 03:23:30 +0530

    Pushing first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,77 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+
+interface IExpenseItem {
+	name: string;
+	price: number;
+	date: number;
+	id: number;
+}
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [item, setItem] = useState<{ name: string; price: number; date: number }>({ name: "", price: 0, date: 0 });
+	const [data, setData] = useState<IExpenseItem[]>([]);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	const getAndSetData = async () => {
+		const dataItem = await fetch("https://api.jsonbin.io/v3/b/65f0ba97dc74654018b1fe10", {
+			headers: { "X-Master-Key": "$2b$10$36wvnzpKZqVs9l1ZsHS2jON.JWV9pBcgmU98Oce5jnCYheagDHuyq" },
+		}).then((res) => res.json());
+		console.log(dataItem);
+		setData(dataItem.record.expenses);
+	};
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	const postDataforExpense = async (dataToPost: IExpenseItem[]) => {
+		const responseData = await fetch("https://api.jsonbin.io/v3/b/65f0ba97dc74654018b1fe10", {
+			method: "PUT",
+			headers: { "X-Master-Key": "$2b$10$36wvnzpKZqVs9l1ZsHS2jON.JWV9pBcgmU98Oce5jnCYheagDHuyq", "Content-Type": "application/json" },
+			body: JSON.stringify({ expenses: dataToPost }),
+		}).then((res) => res.json());
+	};
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+	useState(() => {
+		getAndSetData();
+	});
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+	return (
+		<div className="flex justify-center gap-10 flex-col m-5 min-w-fit">
+			<div className="flex gap-2 w-full overflow-auto ">
+				<label>What is your expense today?</label>
+				<input type="text" onChange={(e) => setItem({ name: e.target.value, price: item.price, date: item.date })}></input>,<label>at what price?</label>
+				<input type="number" onChange={(e) => setItem({ name: item.name, price: parseInt(e.target.value), date: item.date })}></input>
+				<label>on?</label>
+				<input type="date" onChange={(e) => setItem({ name: item.name, price: item.price, date: new Date(e.target.value).getTime() })}></input>
+				<button
+					onClick={() => {
+						const customItem = { ...item, id: data.length + 1 };
+						setData([...data, customItem]);
+						postDataforExpense([...data, customItem]);
+					}}>
+					Add
+				</button>
+			</div>
+			<div className="flex gap-20px w-full overflow-auto">
+				<div className="flex flex-col font-extrabold text-[30px] gap-10 min-w-fit m-5">
+					<div>Name</div>
+					<div>Price</div>
+					<div>Days</div>
+					<div>Current Daily Price</div>
+				</div>
+				<div className="flex gap-10">
+					{data?.map((d) => (
+						<div key={d.id} className="flex flex-col min-w-fit gap-10 text-[30px] m-5 text-black">
+							<div>{d.name}</div>
+							<div>{d.price}</div>
+							<div>{Math.round((new Date().getTime().valueOf() - d.date.valueOf()) / 86400000)}</div>
+							<div>{d.price / Math.round((new Date().getTime().valueOf() - d.date.valueOf()) / 86400000)}</div>
+						</div>
+					))}
+				</div>
+			</div>
+			<div>
+				{`Today's Daily Expense Rate: `}
+				<span>{data.reduce((accumulator, d) => accumulator + d.price / Math.round((new Date().getTime().valueOf() - d.date.valueOf()) / 86400000), 0)}</span>
+			</div>
+		</div>
+	);
 }
